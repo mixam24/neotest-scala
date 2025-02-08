8069014beada8ed972ccb8b8e1b31fa974d564d6
Author <6862019+mixam24@users.noreply.github.com>
2025-02-08 14:33:02 +0100

    feat(flatspec): add flatspec support
    credits to aleix

--- lua/neotest-scala/init.lua
+++ lua/neotest-scala/init.lua
@@ -72,6 +72,45 @@ function ScalaNeotestAdapter.discover_positions(path)
         function: (identifier) @func_name (#match? @func_name "test")
         arguments: (arguments (string) @test.name))
       )) @test.definition
+
+      ((infix_expression
+        left: (infix_expression
+          left: (identifier) @test.describe
+          right: (string) @test.name)
+        operator: (identifier) @keyword.in (#eq? @keyword.in "in")
+        right: (block) @test.body
+      )) @test.definition
+      ((infix_expression
+        left: (infix_expression
+          left: (string) @test.describe
+          right: (string) @test.name)
+        operator: (identifier) @keyword.in (#eq? @keyword.in "in")
+        right: (block) @test.body
+      )) @test.definition
+      ((infix_expression
+        left: (infix_expression
+          left: (string) @test.describe
+          operator: (identifier) @keyword.ignore (#eq? @keyword.ignore "ignore")
+        )
+      )) @test.ignore
+      ((call_expression
+        function: (identifier) @keyword.pending (#eq? @keyword.pending "pending")
+      )) @test.pending
+      ((call_expression
+        function: (identifier) @keyword.cancel (#eq? @keyword.cancel "cancel")
+      )) @test.cancel
+      ((call_expression
+        function: (identifier) @keyword.info (#eq? @keyword.info "info")
+        arguments: (arguments (string) @test.additional_info)
+      )) @test.info
+      ((infix_expression
+        left: (infix_expression
+          left: (string) @test.describe
+          operator: (identifier) @keyword.which (#eq? @keyword.which "which")
+          right: (string) @test.name)
+        operator: (identifier) @keyword.in (#eq? @keyword.in "in")
+        right: (block) @test.body
+      )) @test.definition_with_which
     ]]
     return lib.treesitter.parse_positions(
         path,
