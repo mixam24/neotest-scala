ddbf5adb2eb5d81f7589039caf1be82e67ff403d
Maksim Marozau <6862019+mixam24@users.noreply.github.com>
2025-02-23 15:55:56 +0100

    test(scala): add dummy project to run unit tests against of

---
+++ tests/test-data/scala2/src/test/scala/scalatest/basic/WordSpec.scala
@@ -0,0 +1,20 @@
+package scala.scalatest.basic
+
+import org.scalatest.wordspec.AnyWordSpec
+
+class WordSpec extends AnyWordSpec {
+
+  "A Set" when {
+    "empty" should {
+      "have size 0" in {
+        assert(Set.empty.size == 0)
+      }
+
+      "produce NoSuchElementException when head is invoked" in {
+        assertThrows[NoSuchElementException] {
+          Set.empty.head
+        }
+      }
+    }
+  }
+}
