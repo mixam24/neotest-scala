788b302f7172d507c6e3732d836c8350500cca5c
Maksim Marozau <6862019+mixam24@users.noreply.github.com>
2025-06-12 22:05:31 +0200

    fix(minit): install scala parser during the build

--- tests/minit.lua
+++ tests/minit.lua
@@ -12,7 +12,10 @@ local opts = minit.busted.setup({
         "lunarmodules/luacov",
         "williamboman/mason-lspconfig.nvim",
         "williamboman/mason.nvim",
-        { "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = { "scala" } } },
+        {
+            "nvim-treesitter/nvim-treesitter",
+            build = ":TSInstall scala",
+        },
         "nvim-neotest/nvim-nio",
         "nvim-lua/plenary.nvim",
         "nvim-neotest/neotest",
